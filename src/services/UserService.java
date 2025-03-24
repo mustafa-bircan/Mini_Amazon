b4a4f03f4a0ba633f2703459ebf7aade8a51093b
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-24 12:16:01 +0300

    feat: Implement UserService with user management functionalities
    - Added UserService class to manage user-related operations.
    - Implemented getUserById method to retrieve a user by their ID.
    - Added addUser method to add a new user to the list.
    - Implemented updateUser method to update user details by ID.
    - Added deleteUser method to remove a user by their ID.
    - The UserService class uses an internal list of users for data storage.
    
    This commit allows for basic user management functionality within the service layer of the application.

---
+++ src/services/UserService.java
@@ -0,0 +1,42 @@
+package services;
+
+import models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+    private List<User> users;
+
+    public UserService() {
+        this.users = new ArrayList<>();
+
+        users.add(new User(1, "mstfbrcn","m@example.com","password123"));
+        users.add(new User(2, "gnctrkcll","g@example.com","password321"));
+    }
+
+    public User getUserById(int id) {
+        return users
+                .stream()
+                .filter(user -> user.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void updateUser(int id, User updateUser) {
+        User user = getUserById(id);
+        if (user != null) {
+            user.setUsername(updateUser.getUsername());
+            user.setEmail(updateUser.getEmail());
+            user.setPassword(updateUser.getPassword());
+        }
+    }
+
+    public void deleteUser(int id) {
+        users.removeIf(user -> user.getId() == id);
+    }
+}
