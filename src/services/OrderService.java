12231a6729dc87b3241f09e8069e8b7549dc11ac
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-24 12:36:02 +0300

    Update OrderService.java

--- src/services/OrderService.java
+++ src/services/OrderService.java
@@ -1,29 +1,34 @@
 package services;
 
+import interfaces.IOrderService;
 import models.Order;
 import models.User;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OrderService {
+public class OrderService implements IOrderService {
     private List<Order> orders;
 
     public OrderService() {
         this.orders = new ArrayList<>();
     }
 
+    @Override
     public List<Order> getAllOrders() {
         return orders;
     }
 
+    @Override
     public Order getOrderById(int id) {
         return orders.stream().filter(order -> order.getId() == id).findFirst().orElse(null);
     }
 
+    @Override
     public void addOrder(Order order) {
         orders.add(order);
     }
 
+    @Override
     public void deleteOrder(int id) {
         orders.removeIf(order -> order.getId() == id);
     }
